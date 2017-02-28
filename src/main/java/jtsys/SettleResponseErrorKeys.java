a595c30f696618930263a242b9e9015ea57c30c3
William L. Thomson Jr <wlt@o-sinc.com>
2017-02-28 04:31:13 -0500

    Settle response error keys enum

---
+++ src/main/java/jtsys/SettleResponseErrorKeys.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2017 Obsidian-Studios, Inc.
+ * Distributed under the terms of the GNU General Public License v3
+ *
+ */
+
+package jtsys;
+
+/**
+ * @author William L. Thomson Jr. <wlt@o-sinc.com>
+ */
+public enum SettleResponseErrorKeys {
+    Batch_Record_Count,
+    Batch_Net_Deposit,
+    Batch_Number,
+    Error_Type,
+    Error_Record_Sequence_Number,
+    Error_Record_Type,
+    Error_Data_Field_Number,
+    Error_Data;
+}
